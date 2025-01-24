090533b3b76415e48f47b4c7597d005c088035e9
davidgalstyancaspeco <david.galstyan@caspeco.se>
2025-01-24 10:53:32 +0100

    update

--- .venv/Uppgift3_v3.py
+++ .venv/Uppgift3_v3.py
@@ -23,7 +23,9 @@ while True:
         print("Det blir " + str(amountSum) + " kr totalt, alltså " + str(amountSum/int(personCount)) +
                   "kr per person. Välkommen åter!")
         break
+
     elif amount.isdigit() and int(amount) >= 0:
         amountSum += int(amount)
+
     else:
         print("Fel, prova ange ett tal")
